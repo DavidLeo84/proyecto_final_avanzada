0bfaebbda721abdaac01dcfdda331ef6243d623a
David <dlromerom@uqvirtual.edu.co>
2024-03-30 15:11:56 -0500

    Se cambia el estado de los negocios por estadoregistro y no como estaba por eestado negocio que es el estado del historial de revisiones

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/ComentarioServicioImpl.java
@@ -0,0 +1,36 @@
+package co.edu.uniquindio.proyecto.servicios.interfaces;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComentarioServicioImpl implements IComentarioServicio {
+    @Override
+    public void crearComentario() throws Exception {
+
+    }
+
+    @Override
+    public void responderComentario() throws Exception {
+
+    }
+
+    @Override
+    public void listarComentariosNegocio() throws Exception {
+
+    }
+
+    @Override
+    public void calcularPromedioCalificaciones() throws Exception {
+
+    }
+
+    @Override
+    public void darMegusta() throws Exception {
+
+    }
+
+    @Override
+    public void calcularCantidadMegusta() throws Exception {
+
+    }
+}
